552a618b4079cdaa8d5f437ab7a3f935995da9fa
PMSValerio <37459155+VileOar@users.noreply.github.com>
2024-10-05 19:05:09 +0100

    integrated stomp

--- src/entities/player/stomp.gd
+++ src/entities/player/stomp.gd
@@ -9,17 +9,9 @@ func _ready() -> void:
 	timer.timeout.connect(_on_lifespan_end)
 
 
-func _process(_delta: float) -> void:
-	queue_redraw()
-
-
 func _on_body_entered(body: Node2D) -> void:
 	body.die()
 
 
 func _on_lifespan_end():
 	queue_free()
-
-
-func _draw() -> void:
-	draw_circle(Vector2.ZERO, 128, Color.RED)
