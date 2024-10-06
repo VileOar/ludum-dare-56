9051fc7978e35cf02e45fef1b2ceaeaf9dc2364e
PMSValerio <37459155+VileOar@users.noreply.github.com>
2024-10-06 18:09:30 +0100

    added stomp timer indicator

--- src/entities/creature/states/rush_state.gd
+++ src/entities/creature/states/rush_state.gd
@@ -3,16 +3,19 @@ class_name RushState
 
 var _dir_changed = false ## whether direction was already changed
 
+var _can_cross_mem: bool
+
 
 func activate():
 	super.activate()
 	_dir_changed = false
+	_can_cross_mem = _controller.get_can_cross_gate()
 	_controller.set_can_cross_gate(true)
 
 
 func deactivate():
 	super.deactivate()
-	_controller.set_can_cross_gate(false)
+	_controller.set_can_cross_gate(_can_cross_mem)
 
 
 func allow_next_state(state: String) -> bool:
